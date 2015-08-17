4b799631a513fa342d6348cfc70e851a1d56b47d
ahardencoopers <ahardencoopers@gmail.com>
2015-08-17 01:26:06 -0500

    First commit

---
+++ keymap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xmodmap -e "keycode 9 = Return"
+xmodmap -e "keycode 134 = Return"
+xmodmap -e "keycode 133 = BackSpace"
+xmodmap -e "keycode 49 = Escape"
+xmodmap -e "keycode 118 = F5"
+xmodmap -e "keycode 78 = F12"
+xmodmap capslock
